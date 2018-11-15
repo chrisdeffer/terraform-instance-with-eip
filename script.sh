6523379a6ca71d6b1dab6e881d48c29e18dd57f9
DESKTOP-S7GAHR1\chris <christopher@moogsoft.com>
2018-11-15 11:42:28 -0500

    removed 'sudo' - not needed

--- script.sh
+++ script.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
-sudo apt-get update -y
-sudo apt-get install apache2 -y
-
- 
+apt-get update -y
+apt-get install apache2 -y
 mkfs -t ext4 /dev/xvdg
 mkdir /opt/mount1
 mount /dev/xvdg /opt/mount1
 echo /dev/xvdg  /opt/mount1 ext4 defaults,nofail 0 2 >> /etc/fstab
-
-sudo cp /var/www/html/index.html /var/www/html/index.html.orig
-sudo rm /var/www/html/index.html
-sudo mkdir -p /opt/mount1/apache/html
-sudo echo "<html><body>Hello AWS World!</body></html>" >> /opt/mount1/apache/html/index.html
-sudo touch /opt/mount1/apache/html/screenshot1.png
-sudo touch /opt/mount1/apache/html/screenshot2.png
-sudo ln -s /opt/mount1/apache/html/index.html /var/www/html/index.html
-sudo ln -s /opt/mount1/apache/html/screenshot1.png /var/www/html/screenshot1.png
-sudo ln -s /opt/mount1/apache/html/screenshot2.png /var/www/html/screenshot2.png
+cp /var/www/html/index.html /var/www/html/index.html.orig
+rm /var/www/html/index.html
+mkdir -p /opt/mount1/apache/html
+echo "<html><body>Hello AWS World!</body></html>" >> /opt/mount1/apache/html/index.html
+touch /opt/mount1/apache/html/screenshot1.png
+touch /opt/mount1/apache/html/screenshot2.png
+ln -s /opt/mount1/apache/html/index.html /var/www/html/index.html
+ln -s /opt/mount1/apache/html/screenshot1.png /var/www/html/screenshot1.png
+ln -s /opt/mount1/apache/html/screenshot2.png /var/www/html/screenshot2.png
